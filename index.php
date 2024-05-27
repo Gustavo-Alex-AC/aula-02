030fa9321964a7f608f8bb9cda420b127d418ff4
Gustavo Chitangua <gchitangua@gmail.com>
2024-05-27 12:14:35 +0100

    2nd commit

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php 
-     $titulo = "Nosso Projecto - SOGA"
+     $titulo = "Nosso Projecto - SOGA";
+     $b = 95;
+     $a = 54;
 
+     $fruits = ["Banana", "Orange", "Mango", "Avocado", "u want some?"]
     ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -11,10 +15,32 @@
     <title><?=$titulo?></title>
 </head>
 <body>
-    <h1>Hello world, HTML</h1>
-    <?php 
-     echo "Hello, world, PHP";
-
-    ?>
+    <h1>
+        
+       
+    
+    </h1>
+    <h2>
+        <?=$fruits [4];?>
+    </h2>
+    <ul>
+        <li style = "list-style: none">Fruits</li>
+        <?php
+            for ($i = 0; $i < count($fruits); $i++):
+        ?>    
+        <li><?= $fruits[$i]?></li>
+        <?php
+            endfor;
+        ?>
+    </ul>
 </body>
 </html>
+
+
+
+
+
+
+
+
+
