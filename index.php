4f11cf7fb56f65a27141773cd31317f63030a943
Gustavo Chitangua <gchitangua@gmail.com>
2024-05-27 11:35:03 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php 
+     $titulo = "Nosso Projecto - SOGA"
+
+    ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?=$titulo?></title>
+</head>
+<body>
+    <h1>Hello world, HTML</h1>
+    <?php 
+     echo "Hello, world, PHP";
+
+    ?>
+</body>
+</html>
